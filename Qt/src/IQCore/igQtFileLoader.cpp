ef2d66e8e3053bcf97ed0d2645ebdeb417985e4e
mky8812 <2819787107@qq.com>
2024-07-13 15:18:15 +0800

    Update qt module's treeView.

--- Qt/src/IQCore/igQtFileLoader.cpp
+++ Qt/src/IQCore/igQtFileLoader.cpp
@@ -41,7 +41,7 @@ void igQtFileLoader::OpenFile(const std::string& filePath)
 	
 
 
-	Q_EMIT EmitMakeCurrent();
+//	Q_EMIT EmitMakeCurrent();
 	auto obj = iGame::FileIO::ReadFile(filePath);
 	SurfaceMesh::Pointer mesh = DynamicCast<SurfaceMesh>(obj);
 
@@ -92,7 +92,8 @@ void igQtFileLoader::OpenFile(const std::string& filePath)
 	//mesh1->GetAttributes()->AddScalars(IG_CELL, cellScalar);
 	m_SceneManager->GetCurrentScene()->AddDataObject(obj);
 	//m_SceneManager->GetCurrentScene()->AddDataObject(obj1);
-	Q_EMIT EmitDoneCurrent();
+
+//	Q_EMIT EmitDoneCurrent();
 	
 
 	this->SaveCurrentFileToRecentFile(QString::fromStdString(filePath));
