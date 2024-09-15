af7db5547b1bdd13049b2e49f4c5e575d2a724ca
qglh <694606585@qq.com>
2024-09-15 23:32:57 +0800

    为Filter对话框添加下拉框控件

--- Qt/src/IQComponents/igQtFilterDialogDockWidget.cpp
+++ Qt/src/IQComponents/igQtFilterDialogDockWidget.cpp
@@ -54,40 +54,112 @@ int igQtFilterDialogDockWidget::addParameter(WidgetType type,
                                              const QString& defaultValue) {
 
     QWidget* widget = nullptr;
-    
-    switch (type) {
-        case QT_LINE_EDIT:
-        {
-            QLineEdit* line = new QLineEdit(this);
-            line->setText(defaultValue);
-            widget = line;
-        }
-        break;
-        case QT_CHECK_BOX: 
-        {
-            QCheckBox* check = new QCheckBox(this);
-            bool value = defaultValue == "true" ? true : false;
-            check->setChecked(value);
-            widget = check;
-        } break;
-        default:
-            break;
-    }
 
+    switch (type) {
+    case QT_LINE_EDIT:
+    {
+        QLineEdit* line = new QLineEdit(this);
+        line->setText(defaultValue);
+        widget = line;
+    }
+    break;
+    case QT_CHECK_BOX:
+    {
+        QCheckBox* check = new QCheckBox(this);
+        bool value = defaultValue == "true" ? true : false;
+        check->setChecked(value);
+        widget = check;
+    } break;
+    case QT_COMBO_BOX:
+    {
+        QComboBox* comboBox = new QComboBox;
+        comboBox->addItem(defaultValue);
+        widget = comboBox;
+    } break;
+    default:
+        break;
+    }
+    
     if (widget == nullptr) { 
         return (-1);
     }
 
     QLabel* label = new QLabel(this);
     label->setText(title);
+
+    Item item{ title, {defaultValue}, type, widget };
+    itemMap[index] = item;
+
+    return addParameter(label, widget);
+}
+
+int igQtFilterDialogDockWidget::addParameter(WidgetType type, 
+    const QString& title, 
+    const std::vector<QString>& defaultValue)
+{
+
+    QWidget* widget = nullptr;
+
+    switch (type) {
+    case QT_LINE_EDIT:
+    {
+        if (defaultValue.size() != 1) {
+            std::cout << "Only one parameter is required" << std::endl;
+            return -1;
+        }
+        QLineEdit* line = new QLineEdit(this);
+        line->setText(defaultValue[0]);
+        widget = line;
+    }
+    break;
+    case QT_CHECK_BOX:
+    {
+        if (defaultValue.size() != 1) {
+            std::cout << "Only one parameter is required" << std::endl;
+            return -1;
+        }
+        QCheckBox* check = new QCheckBox(this);
+        bool value = defaultValue[0] == "true" ? true : false;
+        check->setChecked(value);
+        widget = check;
+    } break;
+    case QT_COMBO_BOX:
+    {
+        if (defaultValue.empty()) {
+            std::cout << "At least one parameter is required" << std::endl;
+            return -1;
+        }
+        QComboBox* comboBox = new QComboBox;
+        for (const auto& item : defaultValue) {
+            comboBox->addItem(item);
+        }
+        widget = comboBox;
+    } break;
+    default:
+        break;
+    }
+
+    if (widget == nullptr) {
+        return (-1);
+    }
+
+    QLabel* label = new QLabel(this);
+    label->setText(title);
+
+    Item item{ title, defaultValue, type, widget };
+    itemMap[index] = item;
+
+    return addParameter(label, widget);
+}
+
+int igQtFilterDialogDockWidget::addParameter(QLabel* label, QWidget* value)
+{
     label->setAlignment(Qt::AlignRight | Qt::AlignCenter);
     label->setMinimumHeight(20);
-    widget->setMinimumHeight(20);
+    value->setMinimumHeight(20);
 
     gridLayout->addWidget(label, index, 0);
-    gridLayout->addWidget(widget, index, 1);
+    gridLayout->addWidget(value, index, 1);
 
-    Item item{title, defaultValue, type, widget};
-    itemMap[index] = item;
     return index++;
 }
