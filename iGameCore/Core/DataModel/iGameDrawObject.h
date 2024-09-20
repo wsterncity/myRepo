040f4c0fe4c46ee6f99a681a4d6acfe398fb6055
qglh <694606585@qq.com>
2024-09-20 21:20:37 +0800

    优化切割功能

--- iGameCore/Core/DataModel/iGameDrawObject.h
+++ iGameCore/Core/DataModel/iGameDrawObject.h
@@ -13,6 +13,31 @@ public:
 	static Pointer New() { return new DrawObject; }
 	IGenum GetDataObjectType() const { return IG_DRAW_OBJECT; }
 	IGsize GetRealMemorySize() override { return 0; };
+	virtual bool GetClipped() { return false; }; // Gets whether this can be clipped.
+	void SetExtentClipping(bool _in) { if (GetClipped()) this->m_Clip.m_Extent.m_Use = _in; }
+	void SetPlaneClipping(bool _in) { if (GetClipped()) this->m_Clip.m_Plane.m_Use = _in; }
+	void SetExtent(double xMin, double xMax, double yMin, double yMax,
+		double zMin, double zMax, bool flip = false) {
+		m_Clip.m_Extent.m_bmin[0] = xMin;
+		m_Clip.m_Extent.m_bmin[1] = yMin;
+		m_Clip.m_Extent.m_bmin[2] = zMin;
+		m_Clip.m_Extent.m_bmax[0] = xMax;
+		m_Clip.m_Extent.m_bmax[1] = yMax;
+		m_Clip.m_Extent.m_bmax[2] = zMax;
+		m_Clip.m_Extent.m_flip = flip;
+		SetExtentClipping(true);
+	}
+	void SetPlane(double ox, double oy, double oz,
+		double nx, double ny, double nz, bool flip = false) {
+		m_Clip.m_Plane.m_origin[0] = ox;
+		m_Clip.m_Plane.m_origin[1] = oy;
+		m_Clip.m_Plane.m_origin[2] = oz;
+		m_Clip.m_Plane.m_normal[0] = nx;
+		m_Clip.m_Plane.m_normal[1] = ny;
+		m_Clip.m_Plane.m_normal[2] = nz;
+		m_Clip.m_Plane.m_flip = flip;
+		SetPlaneClipping(true);
+	}
 protected:
 	DrawObject() {};
 	~DrawObject() override = default;
